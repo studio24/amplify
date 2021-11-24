24d13e0f4263206e7c0c4fc04b85b6557a273938
Nicola Saunders <nicola@studio24.net>
2021-11-24 11:04:38 +0000

    Remove JS accessible-autocomplete files specific to W3C project

--- assets/js/working-group-autocomplete.js
+++
@@ -1,7 +0,0 @@
-import simpleAccessibleAutocomplete from './package-extensions/s24-autocomplete';
-
-if (document.documentElement.classList.contains('js')) {
-
-	simpleAccessibleAutocomplete('filter-wg');
-
-}
