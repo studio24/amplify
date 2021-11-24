53337bbc72185ac53659cc852dce7a6a17b9d088
Nicola Saunders <nicola@studio24.net>
2021-11-24 16:40:12 +0000

    Remove file-specific helper function

--- src/js/main/form-error-summary.js
+++ src/js/main/form-error-summary.js
@@ -1,10 +1,5 @@
 var formErrorSummary = (function () {
 
-	// Helper: Check whether element exists
-	function exists(elem) {
-		return (elem != null && (elem.length >= 0 || elem.innerHTML.length >= 0) )
-	}
-
 	let errorSummary = document.querySelector('[data-component="error-summary"]');
 
 	if (exists(errorSummary)) {
