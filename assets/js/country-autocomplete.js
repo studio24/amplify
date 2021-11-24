05589ddcde3f8ba7244c51e2896b30d9cfccad2b
Nicola Saunders <nicola@studio24.net>
2021-11-24 11:03:27 +0000

    Rename directory

--- assets/js/country-autocomplete.js
+++ assets/js/country-autocomplete.js
@@ -1,4 +1,4 @@
-import simpleAccessibleAutocomplete from './libraries-extensions/s24-autocomplete';
+import simpleAccessibleAutocomplete from './package-extensions/s24-autocomplete';
 
 if (document.documentElement.classList.contains('js')) {
 
