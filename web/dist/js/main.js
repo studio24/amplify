8513cf40f35552c1c6663fb7cef2843e2e78a587
Marie Manandise <marie@studio24.net>
2022-01-28 10:38:48 +0000

    watch fonts command

---
+++ web/dist/js/main.js
@@ -0,0 +1,436 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "collapsibles": function() { return /* binding */ collapsibles; }
+/* harmony export */ });
+/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+/**
+ * Collapsible panels
+ * @see https://heydon.github.io/inclusive-components-demos/collapsible-sections/progressive.html
+ * @see https://insidegovuk.blog.gov.uk/2021/10/29/how-we-made-the-gov-uk-accordion-component-more-accessible/
+ */
+
+var collapsibles = (function () {
+
+	// Get all the collapsible containers
+	var collapseArray = Array.prototype.slice.call(document.querySelectorAll('[data-component="collapsibles"]'));
+
+	if ((0,_helpers__WEBPACK_IMPORTED_MODULE_0__.exists)(collapseArray)) {
+
+		// Loop through containers
+		collapseArray.forEach(function (item) {
+
+			// Get headings inside a collapsible container
+			var headingsArray = Array.prototype.slice.call(item.querySelectorAll('[data-heading="collapsibles"]'));
+
+			// Loop through headings
+			headingsArray.forEach(function (heading, index) {
+
+				// Insert a button for opening/closing the collapsible section
+				heading.innerHTML = '<button class="button--ghost" aria-expanded="false">' +
+					'<span class="js-collapsible-heading">' + heading.innerHTML + '</span>' +
+					'<span class="js-collapsible-toggle"><span class="visuallyhidden">, </span>' +
+					'<span class="with-icon--before"><svg class="icon icon--larger" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" focusable="false" aria-hidden="true"><path fill="none" d="M0 0h24v24H0z"/><path d="M15.08 9.59 12 12.67 8.92 9.59 7.5 11l4.5 4.5 4.5-4.5-1.42-1.41z" class="circle-down"/><path d="m12 9-4.5 4.5 1.41 1.41L12 11.83l3.09 3.09 1.41-1.411z" class="circle-up"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg><span class="js-collapsible-label">Show</span><span class="visuallyhidden"> this section</span></span>' +
+					 '</span></button>';
+
+				// Add appropriate aria role to the collapsible section
+				heading.nextElementSibling.setAttribute('aria-hidden', 'true');
+
+				// Assign the button
+				var btn = heading.querySelector('button');
+				var toggleText = btn.querySelector('.js-collapsible-label');
+
+				// Add click event listener
+				btn.addEventListener('click', function(event){
+					// Cast the state as a boolean
+					var expanded = btn.getAttribute('aria-expanded') === 'true';
+
+					// Switch the state
+					btn.setAttribute('aria-expanded', !expanded);
+
+					// Switch the collapsible section's visibility
+					heading.nextElementSibling.setAttribute('aria-hidden', expanded);
+
+					// Update the toggle text
+					if (expanded == true) {
+						toggleText.textContent = 'Show';
+					} else {
+						toggleText.textContent = 'Hide';
+					}
+				});
+
+			}); // End loop
+
+		}); // End loop
+
+	} // End if statement
+
+})();
+
+
+
+/***/ }),
+/* 2 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "exists": function() { return /* binding */ exists; }
+/* harmony export */ });
+/**
+ * Helper functions for use across other JS files
+ */
+
+/**
+ * Check whether an element exists in the DOM
+ * @param elem
+ * @return {boolean}
+ */
+
+var exists = function(elem) {
+	return (elem !== 'undefined' && elem !== null && (elem.length >= 0 || elem.innerHTML.length >= 0))
+};
+
+
+
+/***/ }),
+/* 3 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "disclosureWidget": function() { return /* binding */ disclosureWidget; }
+/* harmony export */ });
+/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+/**
+ * Simple disclosure widget
+ * @see https://adrianroselli.com/2020/05/disclosure-widgets.html
+ */
+
+var disclosureWidget = (function () {
+
+	var toggleButtonArray = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="true"]'));
+
+	var closeDisclosures = function () {
+
+		toggleButtonArray.forEach(function (btn) {
+
+			if (btn.getAttribute('aria-expanded') === 'true') {
+
+				btn.setAttribute('aria-expanded', 'false');
+
+			}
+
+		});
+
+	}
+
+	if ((0,_helpers__WEBPACK_IMPORTED_MODULE_0__.exists)(toggleButtonArray)) {
+
+		toggleButtonArray.forEach(function (btn) {
+
+			btn.style = "";
+			btn.setAttribute('aria-expanded', 'false');
+
+		});
+
+		document.addEventListener('click', function (event) {
+
+			if (event.target.matches('[data-toggle="true"]')) {
+
+				if (event.target.matches('[aria-expanded="false"]')) {
+
+					closeDisclosures();
+					event.target.setAttribute('aria-expanded', 'true');
+
+				} else {
+
+					event.target.setAttribute('aria-expanded', 'false');
+
+				}
+
+			} else {
+
+				closeDisclosures();
+
+			}
+
+		});
+
+		document.addEventListener('keyup', function (event) {
+
+			if (event.defaultPrevented) {
+				return;
+			}
+
+			let key = event.key || event.keyCode;
+
+			if (key === 'Escape' || key === 'Esc' || key === 27) {
+
+				closeDisclosures();
+
+			}
+
+		});
+
+	}
+
+})();
+
+
+
+/***/ }),
+/* 4 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "formErrorSummary": function() { return /* binding */ formErrorSummary; }
+/* harmony export */ });
+/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+/**
+ * Shift focus to form error summary, if present
+ * @see https://design-system.service.gov.uk/components/error-summary/#how-it-works
+ */
+var formErrorSummary = (function () {
+
+	var errorSummary = document.querySelector('[data-component="error-summary"]');
+
+	if ((0,_helpers__WEBPACK_IMPORTED_MODULE_0__.exists)(errorSummary)) {
+		errorSummary.focus();
+	}
+
+})();
+
+
+
+/***/ }),
+/* 5 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "cardEnhancement": function() { return /* binding */ cardEnhancement; }
+/* harmony export */ });
+/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+/**
+ * Card enhancement to trigger the main link whenever the card area is clicked
+ * @see https://css-tricks.com/block-links-the-search-for-a-perfect-solution/
+ */
+
+var cardEnhancement = (function () {
+
+	var cardsArray = Array.prototype.slice.call(document.querySelectorAll('[data-component="card"]'));
+
+	if ((0,_helpers__WEBPACK_IMPORTED_MODULE_0__.exists)(cardsArray)) {
+
+		// Loop through cards adding a click event and identifying the main link
+		cardsArray.forEach(function (card, index) {
+
+			var mainLink = card.querySelector('.card__link');
+			var clickableElems = Array.prototype.slice.call(card.querySelectorAll('[data-click]'));
+
+			// Allow other links/buttons in the card to still be "clickable"
+			if (clickableElems) {
+
+				clickableElems.forEach(function (elem) {
+					return elem.addEventListener("click", function (event) {
+						return event.stopPropagation();
+					});
+				});
+
+			}
+
+			card.addEventListener('click', function() {
+
+				var noTextSelected = !window.getSelection().toString();
+				if (noTextSelected) {
+
+					mainLink.click();
+
+				}
+
+			});
+
+		});
+
+	}
+
+})();
+
+
+
+/***/ }),
+/* 6 */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "responsiveTables": function() { return /* binding */ responsiveTables; }
+/* harmony export */ });
+/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+/**
+ * Responsive tables
+ * Tab index changed from 0 to -1 if there is no horizontal overflow
+ * @see https://www.tpgi.com/short-note-on-improving-usability-of-scrollable-regions/
+ */
+
+var responsiveTables = function () {
+
+	// Get all the table wraps
+	let tablesArray = Array.prototype.slice.call(document.querySelectorAll('.table-wrap'));
+
+	if ((0,_helpers__WEBPACK_IMPORTED_MODULE_0__.exists)(tablesArray)) {
+
+		// Loop through them
+		tablesArray.forEach(function (item) {
+
+			// Get the parent element of the table wrap, and it's width
+			var container = item.parentElement;
+			var containerWidth = parseInt(window.getComputedStyle(container, null).getPropertyValue("width"), 10);
+
+			// Get the table inside the table wrap, and it's width
+			var table = item.firstElementChild;
+			var tableWidth = parseInt(window.getComputedStyle(table, null).getPropertyValue("width"), 10);
+
+			// Comparison: true if the container is wider than the table
+			var noScroll = containerWidth >= tableWidth;
+			var ariaLabel = item.querySelector('caption').id;
+
+			// Only make the container focusable if it needs scrolling
+			if (noScroll === true) {
+
+				item.removeAttribute('role');
+				item.removeAttribute('aria-labelledby')
+				item.removeAttribute('tabindex');
+
+			} else {
+
+				item.setAttribute('role', 'region');
+				item.setAttribute('aria-labelledby', ariaLabel);
+				item.setAttribute('tabindex', '0');
+
+			}
+
+		}); // End loop
+
+	} // End if statement
+
+};
+
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _main_collapsibles__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _main_disclosure_widget__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
+/* harmony import */ var _main_form_error_summary__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+/* harmony import */ var _main_cards__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
+/* harmony import */ var _main_responsive_tables__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(6);
+
+
+
+
+
+
+(0,_main_responsive_tables__WEBPACK_IMPORTED_MODULE_4__.responsiveTables)();
+
+// Tie the responsiveTables function to a resize event, and debounce for performance
+var timeout;
+
+window.addEventListener('resize', function (event) {
+
+	// If timer is null, reset it to 66ms and run desired functions.
+	// Otherwise, wait until timer is cleared
+	if (!timeout) {
+		timeout = setTimeout(function () {
+
+			// Reset timeout
+			timeout = null;
+
+			// Run our resize functions
+			(0,_main_responsive_tables__WEBPACK_IMPORTED_MODULE_4__.responsiveTables)();
+
+		}, 66);
+	}
+
+}, false);
+
+}();
+/******/ })()
+;
