c0a2cc03fbf1dcb8f1b479deec876aa0da420552
Miro Ostafiński <ostafinskim@gmail.com>
2023-08-08 14:17:48 +0100

    sortable-tables move from main directory & collapsibles restore

--- assets-src/js/main.js
+++ assets-src/js/main.js
@@ -1,16 +1,17 @@
 import {exists} from "./main/_exists.helper";
+
 import {cardEnhancement} from "./main/cards";
+import {collapsibles} from "./main/collapsibles";
 import {disclosureWidget} from "./main/disclosure-widget";
 import {formErrorSummary} from "./main/form-error-summary";
 import {responsiveTables} from "./main/responsive-tables";
-import {sortableTable} from "./main/sortable-tables";
 
 function domLoadedActions() {
 	cardEnhancement();
+	collapsibles();
 	disclosureWidget();
 	formErrorSummary();
 	responsiveTables();
-	sortableTable();
 
 	/* Create a navSingleLevel object and initiate single-level navigation for a <ul> with the correct data-component attribute */
 	const navExampleSingle = document.querySelector('ul[data-component="nav-single"]');
@@ -54,24 +55,3 @@ if (document.readyState === 'loading') {
 	// `DOMContentLoaded` has already fired
 	domLoadedActions();
 }
-
-// Tie the responsiveTables function to a resize event, and debounce for performance
-var timeout;
-
-window.addEventListener('resize', function (event) {
-
-	// If timer is null, reset it to 66ms and run desired functions.
-	// Otherwise, wait until timer is cleared
-	if (!timeout) {
-		timeout = setTimeout(function () {
-
-			// Reset timeout
-			timeout = null;
-
-			// Run our resize functions
-			responsiveTables();
-
-		}, 66);
-	}
-
-}, false);
