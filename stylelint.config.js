e3225b7d2029f9cdef5326c975edc64efabcf0e3
Nicola Saunders <nicola@studio24.net>
2025-02-24 12:28:08 +0000

    Modify regex linting rule supporting BEM class names

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,23 @@
 /** @type {import('stylelint').Config}
  *
- * The following CSS rules have been disabled:
- * color-hex-length
- * comment-whitespace-inside rule
+ * Starting rulesets:
+ * - https://stylelint.io/user-guide/rules
+ * - https://github.com/stylelint-scss/stylelint-scss?tab=readme-ov-file#list-of-rules
  *
- * The following SCSS rules have been disabled:
- * scss/at-extend-no-missing-placeholder
- * scss/dollar-variable-colon-space-after
- * scss/dollar-variable-empty-line-before
- * scss/dollar-variable-pattern
- * scss/double-slash-comment-empty-line-before
+ * The following CSS rules have been completely disabled:
+ * color-hex-length (Specify short or long notation for hex colors)
+ * comment-whitespace-inside (Require or disallow whitespace on the inside of comment markers)
  *
- * A custom rule has been added to allow classes using the BEM syntax - see https://github.com/humanmade/coding-standards/issues/193#issuecomment-1405099508
+ * The following SCSS rules have been completely disabled:
+ * scss/at-extend-no-missing-placeholder (Disallow at-extends (@extend) with missing placeholders)
+ * scss/dollar-variable-colon-space-after (Require or disallow whitespace after the colon in $-variable declarations)
+ * scss/dollar-variable-empty-line-before (Require an empty line or disallow empty lines before $-variable declarations.)
+ * scss/dollar-variable-pattern (Specify a pattern for Sass-like variables)
+ * scss/double-slash-comment-empty-line-before (Require or disallow an empty line before //-comments)
+ *
+ * A custom regex-based rule has been added to allow classes using BEM syntax.
+ * Modified from https://github.com/humanmade/coding-standards/issues/193#issuecomment-1405099508
+ * to support our BEM implementation (which is a bit less strict than some)
  * */
 export default {
 	extends: ["stylelint-config-standard-scss"],
@@ -23,9 +29,9 @@ export default {
 		"scss/dollar-variable-empty-line-before": null,
 		"scss/dollar-variable-pattern": null,
 		"scss/double-slash-comment-empty-line-before": null,
-		/** selector class pattern must match [BEM CSS](https://en.bem.info/methodology/css) - [Regex](https://regexr.com/3apms) */
-		'selector-class-pattern': [
-			'^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+		/** selector class pattern must match [BEM CSS](https://en.bem.info/methodology/css) */
+		"selector-class-pattern": [
+			"^[a-z]([-]?[a-z0-9]+)*((__)?(--)?[a-z0-9]([-]?[a-z0-9]+)*)*$",
 			{
 				/** This option will resolve nested selectors with & interpolation. - https://stylelint.io/user-guide/rules/selector-class-pattern/#resolvenestedselectors-true--false-default-false */
 				resolveNestedSelectors: true,
